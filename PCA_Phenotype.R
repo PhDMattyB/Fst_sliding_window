0f00a168779945980660d6d80e1f1f70303c7b9d
Brachmann <Matthew.Brachmann@dfo-mpo.gc.ca>
2022-04-04 21:32:50 -0300

    New code
    Calculation of genomic parallelism and a PCA of phenotypic data

---
+++ PCA_Phenotype.R
@@ -0,0 +1,17 @@
+##################################################################
+## Body shape PCA
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 2022-04-04
+##
+##################################################################
+
+## Load the data manipulation work horse
+library(tidyverse)
+
+## Load other packages 
+
+
+## Read in the data
+
