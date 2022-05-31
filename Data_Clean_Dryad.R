d624d484e7c12810311be84327bdfbaed603c8e1
Brachmann <Matthew.Brachmann@dfo-mpo.gc.ca>
2022-05-30 21:31:01 -0300

    Create Data_Clean_Dryad.R

---
+++ Data_Clean_Dryad.R
@@ -0,0 +1,31 @@
+##################################################################
+## Fixing data frames for submission to dryad
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 2022-05-30
+##
+##################################################################
+
+## Load the data manipulation work horse
+library(tidyverse)
+
+setwd('~/PhD_Genomics_Chapter3/Fst_Iceland_pops/')
+
+GSBPI = read_csv('GSBPI_RDA_outlier_clean.csv') %>% 
+  mutate(BP_Morph_Pair = 'Galtabol_BP')
+SLGBPL = read_csv('SLGBPL_RDA_outlier_clean.csv') %>% 
+  mutate(BP_Morph_Pair = 'Svinavatn_BP')
+TSBPL = read_csv('TSBPL_RDA_outlier_clean.csv') %>% 
+  mutate(BP_Morph_Pair = 'Thingvallavatn_BP2')
+TLGBPL = read_csv('TLGBPL_RDA_outlier_clean.csv') %>% 
+  mutate(BP_Morph_Pair = 'Thingvallavatn_BP1')
+
+RDA_outliers_clean = bind_rows(GSBPI, 
+                               SLGBPL, 
+                               TSBPL, 
+                               TLGBPL)
+
+##test
+RDA_outliers_clean %>% 
+  filter(BP_Morph_Pair == 'Thingvallavatn_BP1')
